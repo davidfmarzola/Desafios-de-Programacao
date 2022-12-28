3fd64cf306f7d77c3b68f3b38ebbc59ac8f31a3f
David Freitas <dvdcomputacao@gmail.com>
2022-12-28 09:02:02 -0300

    solução do problema do programa dti digital

--- dtidigital/petshop_banho_menorpreco.py
+++ dtidigital/petshop_banho_menorpreco.py
@@ -3,22 +3,16 @@
 def indicedodia(data):
     from datetime import date
     dia, mes, ano = data.split('/')
-    dia = int(dia)
-    mes = int(mes)
-    ano = int(ano)
-    data = date(year=ano, month=mes, day=dia)
+    data = date(year=int(ano), month=int(mes), day=int(dia))
     return data.weekday()
 
 def custototaldecadacanil(indicedasemana, caespequenos, caesgrandes):
-    custo = []
+    custo = [] 
     meucaninofeliz = 0
     vairex = 0
-    # 03/08/2018-> 3*30+5*45=90+225=315
     chowchawgas = (caespequenos * 30) + (caesgrandes * 45) # preço é o mesmo, independente do dia
     if indicedasemana >=0 and indicedasemana <= 4: #dia de semana
-        # 03/08/2018-> 3*20+5*40=60+200=260
         meucaninofeliz = (caespequenos*20)+(caesgrandes*40)
-        # 03/08/2018-> 3*15+5*50=45+250=295
         vairex = (caespequenos*15)+(caesgrandes*50)
     else: #final de semana
         meucaninofeliz = (caespequenos*24)+(caesgrandes*48)
@@ -29,7 +23,6 @@ def custototaldecadacanil(indicedasemana, caespequenos, caesgrandes):
     return custo
 
 def petshopescolhido(custo):
-    print()
     if custo[1]<=custo[0] and custo[1]<custo[2]:
         print('Vai Rex | R$'+str(custo[1]))
     elif custo[2]<=custo[1] and custo[2]<=custo[0]:
